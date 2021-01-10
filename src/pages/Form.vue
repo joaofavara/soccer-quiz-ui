492d595d484953bc2ef8aa42211fa714d50ef456
joaofavara <joaopedro_favara@hotmail.com>
2021-01-10 11:44:16 -0300

    feat: create the page for the form

---
+++ src/pages/Form.vue
@@ -0,0 +1,17 @@
+<template>
+	<QuestionsForm />
+</template>
+
+<script>
+import QuestionsForm from "../components/QuestionsForm.vue";
+
+	export default {
+		components: {
+			QuestionsForm,
+		}
+	}
+</script>
+
+<style>
+
+</style>
