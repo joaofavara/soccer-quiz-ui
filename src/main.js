6390a72cd8436c329160ef84dab363a2c939b81c
joaofavara <joaopedro_favara@hotmail.com>
2021-01-11 07:09:59 -0300

    fix: change the theme

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import store from "./store";
 import VueMaterial from "vue-material";
 import "vue-material/dist/vue-material.min.css";
-import "vue-material/dist/theme/default-dark.css";
+import "vue-material/dist/theme/black-green-light.css";
 
 Vue.use(VueMaterial);
 
