1768021b20b71f3fd9ce239f6336c97b53bbb929
joaofavara <joaopedro_favara@hotmail.com>
2021-01-11 07:10:36 -0300

    feat: switch button to nacional or international

--- src/components/QuestionsForm.vue
+++ src/components/QuestionsForm.vue
@@ -1,12 +1,25 @@
 <template>
 	<div>
 		<h1>QuestionsForm</h1>
+		<md-switch v-model="boolean" class="md-primary">
+			<span v-if="boolean">Futebol Nacional</span>
+			<span v-else>Futebol Internacional</span>
+		</md-switch>
 	</div>
 </template>
 
 <script>
 	export default {
-
+		name: 'QuestionsForm',
+		data() {
+			return {
+				// array: [],
+				boolean: false,
+				// string: null,
+				// novalue: null,
+				// disabled: true
+			}
+		},
 	}
 </script>
 
